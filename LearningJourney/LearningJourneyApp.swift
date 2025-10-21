fb7a862f6149dafdd1bdb2d69772f3a54329462e
Bushrahatim8 <bushrahatim8@gmail.com>
2025-10-21 16:55:22 +0300

    Initial Commit

---
+++ LearningJourney/LearningJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningJourneyApp.swift
+//  LearningJourney
+//
+//  Created by Bushra Hatim Alhejaili on 21/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct LearningJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
